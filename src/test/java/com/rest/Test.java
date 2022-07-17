1a7023b47557e48eedd83b26d0d289b99aab1fd3
Amit Panda <amitpanda007@gmail.com>
2022-07-17 20:08:02 +0530

    setup rest asssured

---
+++ src/test/java/com/rest/Test.java
@@ -0,0 +1,11 @@
+package com.rest;
+
+import static io.restassured.RestAssured.*;
+
+public class Test {
+
+    @org.testng.annotations.Test
+    public void login() {
+        given().when().that();
+    }
+}
