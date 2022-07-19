1723db2e1a13cd5bcd59f3cf00948d0f86de9f10
amitpanda007 <amitpanda007@gmail.com>
2022-07-19 09:38:25 +0530

    sample test case

--- src/test/java/com/rest/Test.java
+++ src/test/java/com/rest/Test.java
@@ -6,6 +6,16 @@ public class Test {
 
     @org.testng.annotations.Test
     public void login() {
-        given().when().that();
+        given().
+                baseUri("http://studyopt1-eng.dvdnp.com").
+                header("authorization", "Basic YW1pdHBhbmRhOlBhNTV3b3Jk").
+                header("command", "login").
+                header("tenantid", "tenant1").
+        when().
+                get("/api/login").
+        then().
+                log().all().
+                assertThat().
+                statusCode(200);
     }
 }
